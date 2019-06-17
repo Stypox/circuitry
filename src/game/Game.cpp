a8736081b583c0628c801637d75876004980e805
Stypox <stypox@pm.me>
2019-06-17 09:41:17 +0200

    Add empty game

---
+++ src/game/Game.cpp
@@ -0,0 +1,7 @@
+#include "Game.hpp"
+
+namespace game {
+
+
+
+} // namespace game
---
+++ src/game/Game.hpp
@@ -0,0 +1,14 @@
+#ifndef _CIRCUITRY_GAME_GAME_HPP_
+#define _CIRCUITRY_GAME_GAME_HPP_
+
+namespace game {
+
+class Game {
+	
+public:
+	Game();
+};
+
+} // namespace game
+
+#endif // _CIRCUITRY_GAME_GAME_HPP_
